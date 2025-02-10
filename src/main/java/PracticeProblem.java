8e8c93bef00b67a47538c8ab4b0b784116b05abc
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-10 20:13:41 +0000

    up to question 6

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -20,34 +20,114 @@ public class PracticeProblem {
 	public static void q1() {
 		//Write question 1 code here
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		
+		System.out.println(Math.abs(num));
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		
+		double value = num/num2;
+		System.out.println(Math.floor(value));
+		System.out.println(Math.ceil(value));
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		
+		System.out.println(Math.round(Math.sqrt(num)));
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		
+		System.out.println(Math.pow(num, num2));
+
 	}
 
 	public static void q5() {
 		//Write question 5 code here
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Input a number: ");
+		double num = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Input another number: ");
+		double num2 = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Input one more number: ");
+		double num3 = in.nextDouble();
+		in.nextLine();
+
+		double value = Math.max(num, num2);
+		double answer = Math.max(value, num3);
+
+		System.out.println(answer);
+
+		double value2 = Math.min(num, num2);
+		double answer2 = Math.min(value2, num3);
+
+		System.out.println(answer2);
+
 	}
 	public static void q6() {
 		//Write question 5 code here
 		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input a sentence: ");
+		String word = in.nextLine();
+
+		System.out.println(word.contains("on"));
+
 	}
 	public static void q7() {
 		//Write question 5 code here
 		
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input the word mango: "):
+		String word = in.nextLine();
+
+		String mango = 
+
 	}
 	public static void q8() {
 		//Write question 5 code here
